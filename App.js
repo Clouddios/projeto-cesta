c43feb706b505619fa3a5424e1e8090d662665bb
Claudio Almeida <claudio132009@gmail.com>
2024-10-09 22:28:18 -0300

    Aula 09-10

--- App.js
+++ App.js
@@ -2,14 +2,15 @@ import React from 'react';
 import { StatusBar } from 'expo-status-bar';
 import {StyleSheet, SafeAreaView, Text, View } from 'react-native';
 
-import Cesta from './src/telas/cesta';
+import Cesta from './src/telas';
+import mock from './src/mocks/cesta';
+
 
 export default function App() {
   return (
     <SafeAreaView >
-      <StatusBar >
-        <Cesta />
-      </StatusBar >
+      <StatusBar />
+        <Cesta {...mock} />
     </SafeAreaView >
   );
 }
